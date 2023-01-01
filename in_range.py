e042b66c450a2a9231b5884679b3a6572eb6aaa9
jamescross813 <crossjames813@gmail.com>
2023-01-01 14:43:39 -0600

    set up function w/args

---
+++ in_range.py
@@ -0,0 +1,8 @@
+# Write your in_range function here:
+def in_range(num1, num2, num3):
+    
+# Uncomment these function calls to test your in_range function:
+#print(in_range(10, 10, 10))
+# should print True
+#print(in_range(5, 10, 20))
+# should print False
