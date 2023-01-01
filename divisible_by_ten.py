d8b1750cb1de445333f7984832fa40f5c532f9aa
jamescross813 <crossjames813@gmail.com>
2023-01-01 13:23:40 -0600

    setup

---
+++ divisible_by_ten.py
@@ -0,0 +1,10 @@
+# Write your divisible_by_ten() function here:
+
+
+
+# Uncomment these print() function calls to test your divisible_by_ten() function:
+
+#print(divisible_by_ten(20))
+# should print True
+#print(divisible_by_ten(25))
+# should print False
