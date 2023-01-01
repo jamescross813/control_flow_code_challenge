3bc33a78f7ff654a8df5a72140051aae1b17d126
jamescross813 <crossjames813@gmail.com>
2023-01-01 13:22:07 -0600

    done

--- twice_as_large.py
+++ twice_as_large.py
@@ -6,7 +6,7 @@ def twice_as_large(a,b):
     else:
         return False
 # Uncomment these function calls to test your twice_as_large function:
-#print(twice_as_large(10, 5))
+print(twice_as_large(10, 5))
 # should print False
-#print(twice_as_large(11, 5))
+print(twice_as_large(11, 5))
 # should print True
