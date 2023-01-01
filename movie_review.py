4d6deae1db4917f224b7c43c657dfa7075a18db8
jamescross813 <crossjames813@gmail.com>
2023-01-01 14:58:03 -0600

    control flow written

--- movie_review.py
+++ movie_review.py
@@ -1,6 +1,11 @@
 # Write your movie_review function here:
-def movie_review(stars):
-
+def movie_review(rating):
+    if(rating<= 5):
+        return "Avoid at all costs!"
+    elif(rating <9):
+        return "This one was fun."
+    else:
+        return "Outstanding!"
 # Uncomment these function calls to test your movie_review function:
 #print(movie_review(9))
 # should print "Outstanding!"
@@ -8,3 +13,7 @@ def movie_review(stars):
 # should print "Avoid at all costs!"
 #print(movie_review(6))
 # should print "This one was fun."
+
+# If the rating is equal to or less than 5, return "Avoid at all costs!"
+# If the rating was less than 9, return "This one was fun."
+# If neither of the if statement conditions were met, return "Outstanding!"
