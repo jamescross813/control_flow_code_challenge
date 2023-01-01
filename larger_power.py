0b70a7c2ba722e71a3c25d1835de4b494793d47c
jamescross813 <crossjames813@gmail.com>
2023-01-01 12:54:19 -0600

    uncommented test calls

--- larger_power.py
+++ larger_power.py
@@ -6,7 +6,7 @@ def large_power(a, b):
     
 
 # Uncomment these function calls to test your large_power function:
-# print(large_power(2, 13))
+print(large_power(2, 13))
 # should print True
-# print(large_power(2, 12))
+print(large_power(2, 12))
 # should print False
