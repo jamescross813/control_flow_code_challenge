362ebd34a593da6298ec0228ad01a6af42ef767e
jamescross813 <crossjames813@gmail.com>
2023-01-01 12:50:26 -0600

    added arguements

--- larger_power.py
+++ larger_power.py
@@ -1,7 +1,10 @@
 # Write your large_power function here:
+def large_function(a, b): 
+
+
 
 # Uncomment these function calls to test your large_power function:
-#print(large_power(2, 13))
+# print(large_power(2, 13))
 # should print True
 #print(large_power(2, 12))
 # should print False
