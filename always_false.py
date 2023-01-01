70c91fe77c19a45bc45005a4dbedd499fda0c7f5
jamescross813 <crossjames813@gmail.com>
2023-01-01 14:54:11 -0600

    done

--- always_false.py
+++ always_false.py
@@ -1,9 +1,13 @@
 # Write your always_false function here:
-
+def always_false(num):
+    if(num!=num):
+        return True
+    return False
+    
 # Uncomment these function calls to test your always_false function:
-#print(always_false(0))
+print(always_false(0))
 # should print False
-#print(always_false(-1))
+print(always_false(-1))
 # should print False
-#print(always_false(1))
+print(always_false(1))
 # should print False
