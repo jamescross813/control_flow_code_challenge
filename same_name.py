bb0dbd0aad42444dfc3b8efa4852e03b42278dc2
jamescross813 <crossjames813@gmail.com>
2023-01-01 14:48:42 -0600

    func setup with args

--- same_name.py
+++ same_name.py
@@ -1,5 +1,6 @@
 # Write your same_name function here:
-
+def same_name(name1, name2):
+    
 # Uncomment these function calls to test your same_name function:
 #print(same_name("Colby", "Colby"))
 # should print True
