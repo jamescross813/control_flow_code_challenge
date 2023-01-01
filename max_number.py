da3b18f00ba0349ecdaf117101b309fa5a9bc2e9
jamescross813 <crossjames813@gmail.com>
2023-01-01 15:00:35 -0600

    set up

---
+++ max_number.py
@@ -0,0 +1,11 @@
+# Write your max_num function here:
+
+# Uncomment these function calls to test your max_num function:
+#print(max_num(-10, 0, 10))
+# should print 10
+#print(max_num(-10, 5, -30))
+# should print 5
+#print(max_num(-5, -10, -10))
+# should print -5
+#print(max_num(2, 3, 3))
+# should print "It's a tie!"
