3717e3d39be493efdc17868bfe582af3c4c29fef
jamescross813 <crossjames813@gmail.com>
2023-01-01 14:58:28 -0600

    done

--- movie_review.py
+++ movie_review.py
@@ -7,13 +7,9 @@ def movie_review(rating):
     else:
         return "Outstanding!"
 # Uncomment these function calls to test your movie_review function:
-#print(movie_review(9))
+print(movie_review(9))
 # should print "Outstanding!"
-#print(movie_review(4))
+print(movie_review(4))
 # should print "Avoid at all costs!"
-#print(movie_review(6))
+print(movie_review(6))
 # should print "This one was fun."
-
-# If the rating is equal to or less than 5, return "Avoid at all costs!"
-# If the rating was less than 9, return "This one was fun."
-# If neither of the if statement conditions were met, return "Outstanding!"
