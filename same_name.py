28605986a8815499b0c2f98d1c89667901bd798a
jamescross813 <crossjames813@gmail.com>
2023-01-01 14:47:56 -0600

    setup

---
+++ same_name.py
@@ -0,0 +1,7 @@
+# Write your same_name function here:
+
+# Uncomment these function calls to test your same_name function:
+#print(same_name("Colby", "Colby"))
+# should print True
+#print(same_name("Tina", "Amber"))
+# should print False
