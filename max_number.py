1dc4372c60243aa665cf91d7be2cb7fb292d7dff
jamescross813 <crossjames813@gmail.com>
2023-01-01 15:01:17 -0600

    basic func with args

--- max_number.py
+++ max_number.py
@@ -1,5 +1,6 @@
 # Write your max_num function here:
-
+def max_num(num1, num2, num3):
+    
 # Uncomment these function calls to test your max_num function:
 #print(max_num(-10, 0, 10))
 # should print 10
