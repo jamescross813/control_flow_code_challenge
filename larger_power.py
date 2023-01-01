3b42c62daf243d4a60de126cc0ccef8d46e9ee2d
jamescross813 <crossjames813@gmail.com>
2023-01-01 12:53:38 -0600

    function name change

--- larger_power.py
+++ larger_power.py
@@ -1,10 +1,10 @@
 # Write your large_power function here:
-def large_function(a, b): 
+def large_power(a, b): 
     result = a**b
-
+    
 
 # Uncomment these function calls to test your large_power function:
 # print(large_power(2, 13))
 # should print True
-#print(large_power(2, 12))
+# print(large_power(2, 12))
 # should print False
