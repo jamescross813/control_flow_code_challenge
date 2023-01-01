4007b67df778ea70b01209ec7d15027a253a8012
jamescross813 <crossjames813@gmail.com>
2023-01-01 12:56:45 -0600

    setup

---
+++ budget.py
@@ -0,0 +1,7 @@
+# Write your over_budget function here:
+
+# Uncomment these function calls to test your over_budget function:
+#print(over_budget(100, 20, 30, 10, 40))
+# should print False
+#print(over_budget(80, 20, 30, 10, 30))
+# should print True
