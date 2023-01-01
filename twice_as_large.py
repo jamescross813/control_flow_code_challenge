de51f1442f698d208cd85e6445846a22e751483f
jamescross813 <crossjames813@gmail.com>
2023-01-01 13:21:47 -0600

    control flow inc

--- twice_as_large.py
+++ twice_as_large.py
@@ -1,7 +1,10 @@
 # Write your twice_as_large function here:
 def twice_as_large(a,b):
     result = b*2
-    
+    if(a>result):
+        return True
+    else:
+        return False
 # Uncomment these function calls to test your twice_as_large function:
 #print(twice_as_large(10, 5))
 # should print False
