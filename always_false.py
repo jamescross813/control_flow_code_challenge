872b54abcbd9e1e78310b5edf732909e9f5b40ec
jamescross813 <crossjames813@gmail.com>
2023-01-01 14:52:15 -0600

    setup

---
+++ always_false.py
@@ -0,0 +1,9 @@
+# Write your always_false function here:
+
+# Uncomment these function calls to test your always_false function:
+#print(always_false(0))
+# should print False
+#print(always_false(-1))
+# should print False
+#print(always_false(1))
+# should print False
