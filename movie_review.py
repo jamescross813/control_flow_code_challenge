4f1323de81fe7d91400d72db651d8b671be95b2e
jamescross813 <crossjames813@gmail.com>
2023-01-01 14:55:13 -0600

    setup

---
+++ movie_review.py
@@ -0,0 +1,9 @@
+# Write your movie_review function here:
+
+# Uncomment these function calls to test your movie_review function:
+#print(movie_review(9))
+# should print "Outstanding!"
+#print(movie_review(4))
+# should print "Avoid at all costs!"
+#print(movie_review(6))
+# should print "This one was fun."
