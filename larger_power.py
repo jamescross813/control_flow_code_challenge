b107ebafac36aef186c2a56918d9f1757d819320
jamescross813 <crossjames813@gmail.com>
2023-01-01 12:54:41 -0600

    added False call

--- larger_power.py
+++ larger_power.py
@@ -3,7 +3,8 @@ def large_power(a, b):
     result = a**b
     if(result>=5000):
         return True
-    
+    else:
+        return False
 
 # Uncomment these function calls to test your large_power function:
 print(large_power(2, 13))
